d5733f1d1a25d93e86562be70fa1a013db3eaa51
Igor <datsenko2802@gmail.com>
2022-10-10 22:37:48 +0300

    add new project in github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The future Sneakers App</h1>
+      <h2>This app will write on React and SCSS</h2>
+    </div>
+  );
+}
+
+export default App;
