2b57bc5070ec4e8a3bc8b63a779b67c5d1a99435
Igor <datsenko2802@gmail.com>
2022-10-28 19:14:34 +0300

    add new card

--- src/App.js
+++ src/App.js
@@ -23,7 +23,109 @@ function App() {
                 </ul>
             </header>
             <div className="content p-40">
-                <h1>Всі кросівки</h1>
+                <h1 className="mb-40">Всі кросівки</h1>
+
+                <div className="d-flex">
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={133}
+                            src="/img/sneakers/1.jpg"
+                            alt="Sneakers"
+                        />
+                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
+                        <div className="d-flex justify-between align-center">
+                            <div className="d-flex flex-column ">
+                                <span>цена:</span>
+                                <b>5000грн</b>
+                            </div>
+
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="Plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={133}
+                            src="/img/sneakers/2.jpg"
+                            alt="Sneakers"
+                        />
+                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
+                        <div className="d-flex justify-between align-center">
+                            <div className="d-flex flex-column ">
+                                <span>цена:</span>
+                                <b>5000грн</b>
+                            </div>
+
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="Plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={133}
+                            src="/img/sneakers/3.jpg"
+                            alt="Sneakers"
+                        />
+                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
+                        <div className="d-flex justify-between align-center">
+                            <div className="d-flex flex-column ">
+                                <span>цена:</span>
+                                <b>5000грн</b>
+                            </div>
+
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="Plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={133}
+                            src="/img/sneakers/4.jpg"
+                            alt="Sneakers"
+                        />
+                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
+                        <div className="d-flex justify-between align-center">
+                            <div className="d-flex flex-column ">
+                                <span>цена:</span>
+                                <b>5000грн</b>
+                            </div>
+
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="Plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
