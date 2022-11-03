5e5725f748a04f35d18fec518b6f9af206120dbf
Igor <datsenko2802@gmail.com>
2022-11-03 23:33:10 +0200

    add total,tax and button in drawer

--- src/App.js
+++ src/App.js
@@ -7,43 +7,60 @@ function App() {
                 <div className="drawer">
                     <h2 className="mb-30">Корзина</h2>
                     {/* Те що є в Корзині */}
-                    <div className="cartItem d-flex align-center mb-20">
-                        <div
-                            style={{
-                                backgroundImage: 'url(/img/sneakers/1.jpg)',
-                            }}
-                            className="cartItemImg"
-                        ></div>
-                        <div className="mr-20 flex">
-                            <p className="mb-5">
-                                Чоловічі кросівки Nike Air Max 270
-                            </p>
-                            <b>5000 грн</b>
+                    <div className="items">
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div
+                                style={{
+                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
+                                }}
+                                className="cartItemImg"
+                            ></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">
+                                    Чоловічі кросівки Nike Air Max 270
+                                </p>
+                                <b>5000 грн</b>
+                            </div>
+                            <img
+                                className="removeBtn"
+                                src="/img/btn-remove.svg"
+                                alt="Remove"
+                            />
                         </div>
-                        <img
-                            className="removeBtn"
-                            src="/img/btn-remove.svg"
-                            alt="Remove"
-                        />
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div
+                                style={{
+                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
+                                }}
+                                className="cartItemImg"
+                            ></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">
+                                    Чоловічі кросівки Nike Air Max 270
+                                </p>
+                                <b>5000 грн</b>
+                            </div>
+                            <img
+                                className="removeBtn"
+                                src="/img/btn-remove.svg"
+                                alt="Remove"
+                            />
+                        </div>
                     </div>
-                    <div className="cartItem d-flex align-center mb-20">
-                        <div
-                            style={{
-                                backgroundImage: 'url(/img/sneakers/1.jpg)',
-                            }}
-                            className="cartItemImg"
-                        ></div>
-                        <div className="mr-20 flex">
-                            <p className="mb-5">
-                                Чоловічі кросівки Nike Air Max 270
-                            </p>
-                            <b>5000 грн</b>
-                        </div>
-                        <img
-                            className="removeBtn"
-                            src="/img/btn-remove.svg"
-                            alt="Remove"
-                        />
+                    <div className="cartTotalBlock">
+                        <ul>
+                            <li>
+                                <span>Разом:</span>
+                                <div></div>
+                                <b>16 499 грн.</b>
+                            </li>
+                            <li>
+                                <span>Податок 5%:</span>
+                                <div></div>
+                                <b>824,95 грн.</b>
+                            </li>
+                        </ul>
+                        <button>Оформити замовлення</button>
                     </div>
                 </div>
             </div>
