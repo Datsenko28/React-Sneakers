cc332387b1eeee7d36f53bcab3c7297facea2b2b
Igor <datsenko2802@gmail.com>
2022-11-16 23:50:08 +0200

    fixed a positioning error 'items'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
 import './index.scss'
-
 function App() {
     return (
         <div className="wrapper clear">
             <div className="overlay">
                 <div className="drawer">
-                    <h2>Корзина</h2>
-                    <div className="cartItem">
-                        <img src="/img/sneakers/1.jpg" alt="Sneakers" />
-                        <div>
-                            <p className="mb-5">
-                                Чоловічі кросівки Nike Air Max 270
-                            </p>
-                            <b>5000 грн</b>
+                    <h2 className="d-flex justify-between mb-30 ">
+                        Корзина
+                        <img
+                            className="cu-p"
+                            src="/img/btn-remove.svg"
+                            alt="Remove"
+                        />
+                    </h2>
+
+                    <div className="items">
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div
+                                style={{
+                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
+                                }}
+                                className="cartItemImg"
+                            ></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">
+                                    Чоловічі кросівки Nike Air Max 270
+                                </p>
+                                <b>5000 грн</b>
+                            </div>
+                            <img
+                                className="removeBtn"
+                                src="/img/btn-remove.svg"
+                                alt="Remove"
+                            />
                         </div>
+                        <div className="cartItem d-flex align-center">
+                            <div
+                                style={{
+                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
+                                }}
+                                className="cartItemImg"
+                            ></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">
+                                    Чоловічі кросівки Nike Air Max 270
+                                </p>
+                                <b>5000 грн</b>
+                            </div>
+                            <img
+                                className="removeBtn"
+                                src="/img/btn-remove.svg"
+                                alt="Remove"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="cartTotalBlock">
+                        <ul>
+                            <li>
+                                <span>Разом</span>
+                                <div></div>
+                                <b>16 499 грн.</b>
+                            </li>
+                            <li>
+                                <span>Податок 5%:</span>
+                                <div></div>
+                                <b> 824,95 грн.</b>
+                            </li>
+                        </ul>
+                        <button className="greenButton">
+                            Оформити замовлення
+                            <img src="/img/arrow.svg" alt="Arrow" />
+                        </button>
                     </div>
                 </div>
             </div>
