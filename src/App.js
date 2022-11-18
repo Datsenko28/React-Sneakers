55d1c28b11fa8b3e03d6a14e349f4c5bbd227615
Igor <datsenko2802@gmail.com>
2022-11-18 16:22:15 +0200

    add react components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './index.scss'
+import Card from './components/Card'
+import Header from './components/Header'
 function App() {
     return (
         <div className="wrapper clear">
-            <div className="overlay">
+            <div style={{ display: 'none' }} className="overlay">
                 <div className="drawer">
                     <h2 className="d-flex justify-between mb-30 ">
                         Корзина
@@ -14,63 +16,6 @@ function App() {
                     </h2>
 
                     <div className="items">
-                        <div className="cartItem d-flex align-center mb-20">
-                            <div
-                                style={{
-                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
-                                }}
-                                className="cartItemImg"
-                            ></div>
-                            <div className="mr-20 flex">
-                                <p className="mb-5">
-                                    Чоловічі кросівки Nike Air Max 270
-                                </p>
-                                <b>5000 грн</b>
-                            </div>
-                            <img
-                                className="removeBtn"
-                                src="/img/btn-remove.svg"
-                                alt="Remove"
-                            />
-                        </div>
-                        <div className="cartItem d-flex align-center mb-20">
-                            <div
-                                style={{
-                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
-                                }}
-                                className="cartItemImg"
-                            ></div>
-                            <div className="mr-20 flex">
-                                <p className="mb-5">
-                                    Чоловічі кросівки Nike Air Max 270
-                                </p>
-                                <b>5000 грн</b>
-                            </div>
-                            <img
-                                className="removeBtn"
-                                src="/img/btn-remove.svg"
-                                alt="Remove"
-                            />
-                        </div>
-                        <div className="cartItem d-flex align-center mb-20">
-                            <div
-                                style={{
-                                    backgroundImage: 'url(/img/sneakers/1.jpg)',
-                                }}
-                                className="cartItemImg"
-                            ></div>
-                            <div className="mr-20 flex">
-                                <p className="mb-5">
-                                    Чоловічі кросівки Nike Air Max 270
-                                </p>
-                                <b>5000 грн</b>
-                            </div>
-                            <img
-                                className="removeBtn"
-                                src="/img/btn-remove.svg"
-                                alt="Remove"
-                            />
-                        </div>
                         <div className="cartItem d-flex align-center mb-20">
                             <div
                                 style={{
@@ -150,26 +95,8 @@ function App() {
                     </div>
                 </div>
             </div>
+            <Header />
 
-            <header className="d-flex justify-between align-center p-40">
-                <div className="d-flex align-center	">
-                    <img width={40} height={40} src="/img/logo.png" />
-                    <div className="headerInfo">
-                        <h3 className="text-uppercase">React Sneakers</h3>
-                        <p className="opacity-5">Магазин кращих кросівок</p>
-                    </div>
-                </div>
-
-                <ul className="d-flex">
-                    <li className="mr-30">
-                        <img width={18} height={18} src="/img/basket.svg" />
-                        <span>1300грн</span>
-                    </li>
-                    <li>
-                        <img width={18} height={18} src="/img/user.svg" />
-                    </li>
-                </ul>
-            </header>
             <div className="content p-40">
                 <div className="d-flex align-center justify-between  mb-40">
                     <h1>Всі кросівки</h1>
@@ -180,109 +107,7 @@ function App() {
                 </div>
 
                 <div className="d-flex">
-                    <div className="card">
-                        <div className="favorite">
-                            <img src="/img/heart-unliked.svg" alt="Unliked" />
-                        </div>
-
-                        <img
-                            width={133}
-                            height={133}
-                            src="/img/sneakers/1.jpg"
-                            alt="Sneakers"
-                        />
-                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className="d-flex flex-column ">
-                                <span>цена:</span>
-                                <b>5000грн</b>
-                            </div>
-
-                            <button className="button">
-                                <img
-                                    width={11}
-                                    height={11}
-                                    src="/img/plus.svg"
-                                    alt="Plus"
-                                />
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img
-                            width={133}
-                            height={133}
-                            src="/img/sneakers/2.jpg"
-                            alt="Sneakers"
-                        />
-                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className="d-flex flex-column ">
-                                <span>цена:</span>
-                                <b>5000грн</b>
-                            </div>
-
-                            <button className="button">
-                                <img
-                                    width={11}
-                                    height={11}
-                                    src="/img/plus.svg"
-                                    alt="Plus"
-                                />
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img
-                            width={133}
-                            height={133}
-                            src="/img/sneakers/3.jpg"
-                            alt="Sneakers"
-                        />
-                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className="d-flex flex-column ">
-                                <span>цена:</span>
-                                <b>5000грн</b>
-                            </div>
-
-                            <button className="button">
-                                <img
-                                    width={11}
-                                    height={11}
-                                    src="/img/plus.svg"
-                                    alt="Plus"
-                                />
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img
-                            width={133}
-                            height={133}
-                            src="/img/sneakers/4.jpg"
-                            alt="Sneakers"
-                        />
-                        <h5>Кросівки Чоловічі Nike Blazer Mid Suede</h5>
-                        <div className="d-flex justify-between align-center">
-                            <div className="d-flex flex-column ">
-                                <span>цена:</span>
-                                <b>5000грн</b>
-                            </div>
-
-                            <button className="button">
-                                <img
-                                    width={11}
-                                    height={11}
-                                    src="/img/plus.svg"
-                                    alt="Plus"
-                                />
-                            </button>
-                        </div>
-                    </div>
+                    <Card />
                 </div>
             </div>
         </div>
