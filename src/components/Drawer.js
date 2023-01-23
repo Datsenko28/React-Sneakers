5a0c65cd1a4bf33875c739883fc1cc5f53f5bfb3
Igor <datsenko2802@gmail.com>
2023-01-23 21:12:59 +0200

    making changes in Drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -15,27 +15,47 @@ const Drawer = ({ onClose, onRemove, items = [] }) => {
                 </h2>
 
                 {items.length > 0 ? (
-                    <div className="items">
-                        {items.map((obj) => (
-                            <div className="cartItem d-flex align-center mb-20">
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${obj.imageUrl})`,
-                                    }}
-                                    className="cartItemImg"
-                                ></div>
-                                <div className="mr-20 flex">
-                                    <p className="mb-5">{obj.title}</p>
-                                    <b>{obj.price}</b>
+                    <div>
+                        <div className="items">
+                            {items.map((obj) => (
+                                <div className="cartItem d-flex align-center mb-20">
+                                    <div
+                                        style={{
+                                            backgroundImage: `url(${obj.imageUrl})`,
+                                        }}
+                                        className="cartItemImg"
+                                    ></div>
+                                    <div className="mr-20 flex">
+                                        <p className="mb-5">{obj.title}</p>
+                                        <b>{obj.price}</b>
+                                    </div>
+                                    <img
+                                        onClick={() => onRemove(obj.id)}
+                                        className="removeBtn"
+                                        src="/img/btn-remove.svg"
+                                        alt="Remove"
+                                    />
                                 </div>
-                                <img
-                                    onClick={() => onRemove(obj.id)}
-                                    className="removeBtn"
-                                    src="/img/btn-remove.svg"
-                                    alt="Remove"
-                                />
-                            </div>
-                        ))}
+                            ))}
+                        </div>
+                        <div className="cartTotalBlock">
+                            <ul>
+                                <li>
+                                    <span>Разом</span>
+                                    <div></div>
+                                    <b>16 499 грн.</b>
+                                </li>
+                                <li>
+                                    <span>Податок 5%:</span>
+                                    <div></div>
+                                    <b> 824,95 грн.</b>
+                                </li>
+                            </ul>
+                            <button className="greenButton">
+                                Оформити замовлення
+                                <img src="/img/arrow.svg" alt="Arrow" />
+                            </button>
+                        </div>
                     </div>
                 ) : (
                     <div className="cartEmty d-flex align-center jastify-center flex-column flex">
@@ -57,25 +77,6 @@ const Drawer = ({ onClose, onRemove, items = [] }) => {
                         </button>
                     </div>
                 )}
-
-                <div className="cartTotalBlock">
-                    <ul>
-                        <li>
-                            <span>Разом</span>
-                            <div></div>
-                            <b>16 499 грн.</b>
-                        </li>
-                        <li>
-                            <span>Податок 5%:</span>
-                            <div></div>
-                            <b> 824,95 грн.</b>
-                        </li>
-                    </ul>
-                    <button className="greenButton">
-                        Оформити замовлення
-                        <img src="/img/arrow.svg" alt="Arrow" />
-                    </button>
-                </div>
             </div>
         </div>
     )
